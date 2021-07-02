8716ddde9150908e29ff7b5d93b4434afde92b15
xiechaokang <xiechaokang@sandboxol.com>
2021-07-02 10:03:23 +0800

    #合并链表-手动循环

--- src/合并两个链表.java
+++ src/合并两个链表.java
@@ -4,6 +4,12 @@
 
 public class 合并两个链表 {
 
+    /**
+     * 递归拼接
+     * @param l1
+     * @param l2
+     * @return
+     */
     public static Node mergeTwoLists(Node l1, Node l2) {
         if (l1 == null){
             return l2;
@@ -20,6 +26,44 @@ public class 合并两个链表 {
         }
     }
 
+    /**
+     * 手动循环拼接
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public static Node merge(Node l1, Node l2){
+        if (l1 == null){
+            return l2;
+        }
+        if (l2 == null){
+            return l1;
+        }
+        Node res = new Node();
+
+        boolean flag = true;
+        while (flag){
+            while (l1 != null && l2 != null){
+                if (l1.val < l2.val){
+                    res.addNode(new Node(l1.val));
+                    l1 = l1.next;
+                }else {
+                    res.addNode(new Node(l2.val));
+                    l2 = l2.next;
+                }
+            }
+            if (l1 != null){
+                res.addNode(l1);
+            }
+            if (l2 != null){
+                res.addNode(l2);
+            }
+            flag = false;
+        }
+
+        return res;
+    }
+
     public static void main(String[] args) {
         int[] l1 = new int[]{1,2,3};
         int[] l2 = new int[]{1,4,5};
@@ -30,8 +74,8 @@ public class 合并两个链表 {
         for (int i : l2) {
             l4.addNode(new Node(i));
         }
-        Node res = mergeTwoLists(l3,l4);
-        res.printNode();
+        Node res = merge(l3.head,l4.head);
+        res.head.printNode();
     }
 }
 
@@ -42,7 +86,7 @@ class Node {
     Node(int val) { this.val = val; }
     Node(int val, Node next) { this.val = val; this.next = next; }
 
-    private Node head;
+    Node head;
 
     public void addNode(Node node){
         Node temp = head;
